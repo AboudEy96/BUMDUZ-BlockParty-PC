179311d2ede4afc3f6dc44e324479a7e50ea3748
Abdulkadir <aboudey96aboudeh@gmail.com>
2025-01-03 18:04:39 +0300

    Player Interface

---
+++ Assets/Scripts/Game Scripts/Player/IPlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public interface IPlayerController
+{
+    bool IsGrounded(Vector3 groundCheckPosition, float groundDistance, LayerMask groundMask);
+    void ResetVerticalVelocity();
+    void Jump(float jumpHeight, float gravity);
+    Vector3 CalculateMoveDirection(Transform cameraTransform, float horizontal, float vertical);
+    void RotateTowards(Vector3 direction, float rotationSpeed);
+    void Move(Vector3 direction, float moveSpeed);
+
+}
---
+++ Assets/Scripts/Game Scripts/Player/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Player : MonoBehaviour
+{ 
+    // MOVE FORCES
+    private float moveSpeed = 5.0f;
+    private float rotationSpeed = 10.0f;
+    
+    // JUMP FORCES
+    private float jumpForce = 2.0f;
+    private float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    [Header("Camera Settings")]
+    public Transform cameraTransform;
+
+    private PlayerController characterController;
+
+    private void Awake()
+    {
+        characterController = new PlayerController(GetComponent<CharacterController>());
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (characterController.IsGrounded(groundCheck.position, groundDistance, groundMask))
+        {
+            characterController.ResetVerticalVelocity();
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                characterController.Jump(jumpForce, gravity);
+            }
+        }
+
+        Vector3 moveDirection = characterController.CalculateMoveDirection(cameraTransform, Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        characterController.RotateTowards(moveDirection, rotationSpeed);
+        characterController.Move(moveDirection, moveSpeed);
+    }
+}
+
