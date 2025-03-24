86870ebbf5921358c662bb529b9a52ff0cacf5f4
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-03-24 03:33:44 +0300

    Games List

--- Assets/Scripts/Game Scripts/MultiPlayer/CreateJoinRooms.cs
+++ Assets/Scripts/Game Scripts/MultiPlayer/CreateJoinRooms.cs
@@ -1,4 +1,6 @@
-﻿using Photon.Pun;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,16 +8,23 @@ public class CreateJoinRooms : MonoBehaviourPunCallbacks
 {
     public InputField create;
     public InputField join;
+    private Dictionary<string, int> roomList = new Dictionary<string, int>();
+    
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(create.text);
+        roomList.Add(create.text, 1);
+        Debug.Log($"created room with name {create.text} with this type of players {roomList[create.text]}");
     }
 
     public void JoinRoom()
     {
         PhotonNetwork.JoinRoom(join.text);
+        roomList[join.text] += 1;
+        Debug.Log($"some player joined to {join.text} with this type of players {roomList[join.text]}");
     }
 
+    [PunRPC]
     public override void OnJoinedRoom()
     {
         ServerOnlinePlayers.addOnlinePlayer();
