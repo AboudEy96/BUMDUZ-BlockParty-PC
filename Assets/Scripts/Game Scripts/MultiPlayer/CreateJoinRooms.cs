49ea81d78d9869b10ca3ec5381a149ef00c75bdd
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-03-28 00:42:22 +0300

    rooms list not working but ok

--- Assets/Scripts/Game Scripts/MultiPlayer/CreateJoinRooms.cs
+++ Assets/Scripts/Game Scripts/MultiPlayer/CreateJoinRooms.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,18 +9,27 @@ public class CreateJoinRooms : MonoBehaviourPunCallbacks
 {
     public InputField create;
     public InputField join;
+
+    public GameObject joinButton;
+    public Transform listContainer;
     private Dictionary<string, int> roomList = new Dictionary<string, int>();
-    
+    [PunRPC]
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(create.text);
-        roomList.Add(create.text, 1);
-        Debug.Log($"created room with name {create.text} with this type of players {roomList[create.text]}");
+        photonView.RPC("CreateRoomButton", RpcTarget.All, create.text);
+    }
+
+    [PunRPC]
+    void CreateRoomButton(string roomName)
+    {
+        GameObject theButtonofRoom = PhotonView.Instantiate(joinButton, listContainer);
+        theButtonofRoom.transform.name = roomName;
     }
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom(join.text);
+        PhotonNetwork.JoinRoom(joinButton.name);
         roomList[join.text] += 1;
         Debug.Log($"some player joined to {join.text} with this type of players {roomList[join.text]}");
     }
@@ -28,7 +38,7 @@ public class CreateJoinRooms : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         ServerOnlinePlayers.addOnlinePlayer();
-        PhotonNetwork.LoadLevel("Game");
-        PhotonNetwork.LocalPlayer.NickName = $"Player-{PhotonNetwork.LocalPlayer.ActorNumber}";
+      PhotonNetwork.LoadLevel("Game");
+       PhotonNetwork.LocalPlayer.NickName = $"Player-{PhotonNetwork.LocalPlayer.ActorNumber}";
     }
 }
