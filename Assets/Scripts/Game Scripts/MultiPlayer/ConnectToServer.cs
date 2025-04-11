eeb27cc1b8cc5f62b297f2740b21f3681633221b
Abdulkadir <aboudey96aboudeh@gmail.com>
2025-04-11 14:53:54 +0300

    Loading Fix and New Room Menu

--- Assets/Scripts/Game Scripts/MultiPlayer/ConnectToServer.cs
+++ Assets/Scripts/Game Scripts/MultiPlayer/ConnectToServer.cs
@@ -1,11 +1,29 @@
-﻿using Photon.Pun;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Image = UnityEngine.UI.Image;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    [Header("The Loading Images")]
+    public List<Sprite> images = new List<Sprite>();
+    
+    public Canvas MainCanvas;
+    public Canvas LoadingCanvas;
+
+    public void SendLoading()
+    {
+        MainCanvas.gameObject.SetActive(false);
+        Image im = LoadingCanvas.GetComponentInChildren<Image>();
+        int randomIndex = Random.Range(0, images.Count);
+        im.sprite = images[randomIndex];
+        LoadingCanvas.gameObject.SetActive(true);
+    }
     public void ConnectUsingSettings()
     {
+        SendLoading();
        PhotonNetwork.ConnectUsingSettings();    
     }
 
