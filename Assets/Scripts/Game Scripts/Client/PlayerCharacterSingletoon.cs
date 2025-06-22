3b9975b4568f01e37d66fbb0a3a6a4ac0f59a6c2
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-06-23 01:29:28 +0300

    BUMDUZ Skeleton colors add

---
+++ Assets/Scripts/Game Scripts/Client/PlayerCharacterSingletoon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerCharacterSingletoon : MonoBehaviour
+{
+    // -- Singletoon to save the player character and color --
+    public static GameObject CHARACTER;
+    public static Texture COLOR;
+    public static Material MAT;
+
+    private void Start()
+    {
+        MAT = CHARACTER.GetComponent<Renderer>().material;
+        MAT.mainTexture = COLOR;
+        
+    }
+    
+}
