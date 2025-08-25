29160306d3b518e7da5b7c2f7bcf7679040280d0
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-08-25 21:02:46 +0300

    add orange character for test

--- Assets/Scripts/Menu Scripts/Button/ButtonClickMangments.cs
+++ Assets/Scripts/Menu Scripts/Button/ButtonClickMangments.cs
@@ -11,9 +11,14 @@ using Image = UnityEngine.UI.Image;
 
 public class ButtonClickMangments : MonoBehaviour,IButtonClickMangment
 {
+    public List<Transform> characters = new List<Transform>();
      public List<Sprite> images = new List<Sprite>();
      public List<GameObject> buttons = new List<GameObject>();
      public Transform theImage;
+     
+     public GameObject PREFAB_PLAYER = PlayerCharacterSingletoon.CHARACTER;
+     public Material PLAYER_SKIN;
+    
      private string currentMode;
      [Header("The Menu of choose object")] public GameObject chooseModeObject;
      public void Awake()
@@ -48,6 +53,7 @@ public class ButtonClickMangments : MonoBehaviour,IButtonClickMangment
                 Image img = theImage.GetComponent<Image>();
                 img.sprite = image;
                 ActiveButtons();
+                ActiveCharacter();
             }
         }
     }
@@ -67,4 +73,19 @@ public class ButtonClickMangments : MonoBehaviour,IButtonClickMangment
         }   
     }
 
+    public void ActiveCharacter()
+    {
+        foreach (Transform character in characters)
+        {
+            character.gameObject.SetActive(character.name.Contains(GetCurrentMode()));
+                //   Vector3 location = new Vector3(2.349438f, -0.5281435f, 2.576072f);
+          /*  PlayerUser player = new PlayerUserBuilder(PREFAB_PLAYER)
+                .SetId(1)
+                .SetName("Your Skin")
+                .SetSkinMaterial(PLAYER_SKIN)
+                .Build(location);*/
+            
+            
+        }
+    }
 }
