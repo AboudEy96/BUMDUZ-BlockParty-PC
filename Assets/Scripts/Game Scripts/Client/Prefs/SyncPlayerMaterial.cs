68678e7a3f290e0a43cb1a1cbad755d7d2eb992f
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-11-07 08:37:41 +0300

    host can see players skin

--- Assets/Scripts/Game Scripts/Client/Prefs/SyncPlayerMaterial.cs
+++ Assets/Scripts/Game Scripts/Client/Prefs/SyncPlayerMaterial.cs
@@ -1,12 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SyncPlayerMaterial : MonoBehaviour
 {
-    
-    public List<Material> materials;
-    private void Start()
-    {
+   public List<Material> _skinMaterials = new List<Material>();
 
-    }
+   public static SyncPlayerMaterial instance;
+   private void Awake()
+   {
+      if (instance != null && instance != this)
+      {
+         Destroy(this.gameObject);
+         return;
+      }
+      
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+   }
+   public SyncPlayerMaterial GetInstance()
+   {
+      return instance;
+   }
+   public Material GetMaterialByName(string name)
+   {
+      foreach (var mat in _skinMaterials)
+      {
+         if (mat.name == name)
+         {
+            return mat;
+         }
+      }
+      return null;
+   }
+
 }
