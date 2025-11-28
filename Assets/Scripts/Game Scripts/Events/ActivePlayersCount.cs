77c949251a5b0c7252538aec1413e3f2afa9dddf
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-11-28 23:17:24 +0300

    player winner add

---
+++ Assets/Scripts/Game Scripts/Events/ActivePlayersCount.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class ActivePlayerCount : MonoBehaviour
+{
+    public static ActivePlayerCount instance;
+    
+    private int aLifePlayers = PhotonNetwork.PlayerList.Length;
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+   //     DontDestroyOnLoad(gameObject);
+    }
+    public int GetActivePlayersCount()
+    {
+        return aLifePlayers;
+    }
+    public void SetActivePlayersCount(int count)
+    {
+        aLifePlayers = count;
+    }
+    public void RemoveActivePlayer()
+    {
+        aLifePlayers--;
+    }
+    public static ActivePlayerCount GetInstance()
+    {
+        return instance;
+    }
+    
+    
+}
---
+++ Assets/Scripts/Game Scripts/Events/PlayerWinEvent.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerWinEvent : MonoBehaviourPunCallbacks
+{
+    public static PlayerWinEvent Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    public void OnPlayerWin(Photon.Realtime.Player pl)
+    {
+        CheckIfPlayerWin();
+        GameStartSingletoon.GetInstance().GameEnd();
+    }
+
+    public void CheckIfPlayerWin()
+    {
+        int SurvivedPlayers = 0;
+        Photon.Realtime.Player lastAlivePlayer = null;
+
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            bool isDead = false;
+            if (player.CustomProperties.ContainsKey("isDead"))
+                isDead = (bool)player.CustomProperties["isDead"];
+
+            if (!isDead)
+            {
+                SurvivedPlayers++;
+                lastAlivePlayer = player;
+            }
+        }
+        
+        if (SurvivedPlayers == 1)
+        {
+            WhoWon(lastAlivePlayer);
+        }
+    }
+
+
+    public void WhoWon(Photon.Realtime.Player winner)
+    {
+        photonView.RPC("PrintWinner", RpcTarget.All, winner.NickName);
+        photonView.RPC("LeaveRoom", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void LeaveRoom()
+    {
+        StartCoroutine(LeaveRoomCoroutine());
+    }
+
+    private IEnumerator LeaveRoomCoroutine()
+    {
+        yield return new WaitForSeconds(7f);
+
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+    [PunRPC]
+    void PrintWinner(string winnerName)
+    {
+        Debug.Log("Winner of the game is: " + winnerName);
+    }
+}
