9eed5d498b69b83167be5d5be84c37aa970cc118
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2025-12-01 16:51:19 +0300

    factory setup for players in room

---
+++ Assets/Scripts/Menu Scripts/LobbyMenu/PlayersFactory/PhotonPlayerFactory.cs
@@ -0,0 +1,18 @@
+﻿using Menu_Scripts.LobbyMenu.PlayersInRoom;
+using Photon.Pun;
+using UnityEngine;
+
+public class PhotonPlayerFactory : MonoBehaviour, IPlayerFactory
+{
+
+    public Transform[] spawnPoints;
+    public IPlayer CreatePlayer(int spawnIndex, SkinInfo skinInfo)
+    {
+        var go = PhotonNetwork.Instantiate(skinInfo.prefab.name,
+            spawnPoints[spawnIndex].position, 
+            spawnPoints[spawnIndex].rotation);
+        var player = go.GetComponent<IPlayer>();
+        player.Initialize(PhotonNetwork.LocalPlayer.ActorNumber, null);
+        return player;
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/LobbyMenu/PlayersFactory/SkinInfo.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+    public class SkinInfo : MonoBehaviour {
+        public string skinName;
+        public GameObject prefab;
+        public Material material;
+}
