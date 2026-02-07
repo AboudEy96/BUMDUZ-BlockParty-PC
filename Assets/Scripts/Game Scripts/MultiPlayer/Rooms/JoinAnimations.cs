69a5ed100fbdafa2c3a4b01efb2ca5ecfaa867d5
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2026-02-08 00:57:36 +0300

    lobby animatino test

---
+++ Assets/Scripts/Game Scripts/MultiPlayer/Rooms/JoinAnimations.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class JoinAnimation : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Animator  anim;
+    public override void OnJoinedRoom()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        anim.SetTrigger("LobbyPlayer2");
+    }
+}
