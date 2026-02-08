d5a0adfc0293ac71e6a43dd5b0a0760a5d987588
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2026-02-08 21:26:00 +0300

    InRoom Players and Animations

--- Assets/Scripts/Game Scripts/MultiPlayer/Rooms/JoinAnimations.cs
+++ Assets/Scripts/Game Scripts/MultiPlayer/Rooms/JoinAnimations.cs
@@ -1,14 +1,40 @@
-﻿using Photon.Pun;
+﻿using System.Collections;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
 public class JoinAnimation : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private Animator  anim;
-    public override void OnJoinedRoom()
+    [SerializeField] private Animator  _anim;
+    [SerializeField] private TextMeshPro _name;
+    IEnumerator Start()
     {
-        if (!photonView.IsMine)
-            return;
+        while (photonView == null || photonView.Owner == null) yield return null;
 
-        anim.SetTrigger("LobbyPlayer2");
+        
+        var player = photonView.Owner;
+        object value = null;
+
+        while (!player.CustomProperties.TryGetValue("SpawnIndex", out value))
+            yield return null;
+
+        int spawnIndex = value switch
+        {
+            int i => i,
+            byte b => b,
+            _ => -1
+        };
+        
+             
+            _name = GetComponentInChildren<TextMeshPro>();
+            _anim = GetComponent<Animator>();
+            
+            _name.text = player.NickName;
+            _anim.SetTrigger($"LobbyPlayer{spawnIndex}");
+            
+            Debug.Log($"Player {spawnIndex} spawned");
+            Debug.Log($"Animation LobbyPlayer{spawnIndex}");
+            Debug.Log($"Player {_name.text} spawned");
     }
+
 }
